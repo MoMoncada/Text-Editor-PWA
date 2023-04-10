c2f8b67b5a0514299327b69670f3367627d607b2
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-04-10 15:45:47 +0700

    worked on webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,18 +11,65 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      //-- Webpack Plugin to inject bundles and generate HTML file --//
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      //-- Service Worker --//
+      new InjectManifest ({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      //-- manifest.json file --//
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Text Editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets','icons')
+          }
+        ]
+      })
     ],
 
     module: {
+      //-- CSS Loaders --//
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          //-- To use ES6 --//
+          use: {
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
         
       ],
     },
