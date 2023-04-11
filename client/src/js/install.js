bffa8f3cc207b286e709e04be57ac97951403417
MoMoncada <116573325+MoMoncada@users.noreply.github.com>
2023-04-11 17:19:33 +0700

    added code to database.js and install.js, comments have been edited and app is running well I believe

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,37 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+//-- retrieves the HTML element with the ID "buttonInstall" and stores it in the `butInstall` constant --//
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('hit')
+    console.log("event" + event)
+    event.preventDefault();
+    // Store the triggered events
+    window.deferredPrompt = event;
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+    
+    butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+
+//-- When the button is clicked, it retrieves the "beforeinstallprompt" event that was stored earlier in the `window.deferredPrompt` property. If the event doesn't exist, the function returns and does nothing. If the event exists, it shows the installation prompt to the user, clears the `window.deferredPrompt` property, and adds the "hidden" class to the "buttonInstall" element to hide it again --//
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    
+    if (!promptEvent) {
+        return;
+    }
+    
+    promptEvent.prompt();
+
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+});
+
+
+//-- When this event is triggered, it logs a message to the console and clears the `window.deferredPrompt` property --//
+window.addEventListener('appinstalled', (event) => {
+    
+    console.log('install hit')
+    window.deferredPrompt = null;
+}); 
